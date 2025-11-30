71ee00c6d616956c05fe580a21a1e663606d4518
jacob victor tendo <Jacob@marascientific.org>
2025-11-30 21:18:02 +0300

    fix o nthe server for the db

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
 import os
 
+# import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 
-        'postgresql://postgres:password1@localhost:5432/sjhardware')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')
+    SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
