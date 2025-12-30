c0435646cb644c2b9a6f8b5fa83d82fc833ea3eb
jacob victor tendo <Jacob@marascientific.org>
2025-12-30 15:57:00 +0300

    fixed

--- sjhardware-frontend/src/components/Sidebar.vue
+++ sjhardware-frontend/src/components/Sidebar.vue
@@ -8,7 +8,7 @@
   >
     <!-- Logo -->
     <div class="p-6 font-bold text-xl border-b border-gray-700 flex justify-between items-center">
-      <span v-if="!collapsed">Retail Service Shop</span>
+      <span v-if="!collapsed">SJ HardWare</span>
       <button @click="toggleSidebar" class="focus:outline-none">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
